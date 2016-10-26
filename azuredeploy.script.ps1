e7e82dd77a2fe5397a46af8da3b8748595de7f12
aqasrawi <aqasrawi@microsoft.com>
2016-10-26 01:00:30 -0400

    g

--- azuredeploy.script.ps1
+++ azuredeploy.script.ps1
@@ -1,31 +1,38 @@
 ﻿param(
  
  
- [Parameter(Mandatory=$true)] 
- [String] $aq  
+# [Parameter(Mandatory=$true)] 
+# [String] $aq  
  
 )
 
-Read-Host "Enter AQ"
-Write-Host $aq
+#Read-Host "Enter AQ"
+#Write-Host $aq
 
 
 
-Login-AzureRmAccount
-Get-AzureRmSubscription
+#Login-AzureRmAccount
+#Get-AzureRmSubscription
 Select-AzureRmSubscription -SubscriptionId 3a4af7b3-b7ac-463d-9940-1d80445961a8
 #Set-AzureRmContext -SubscriptionID 3a4af7b3-b7ac-463d-9940-1d80445961a8
 
-$srNumber = 20161
+#$srNumber = Read-Host ("SR Number ") # 'm2016'
+$srNumber = 'd2016'
+
 $resourceGroupName = 'RGsp' + $srNumber
 $location = 'east us 2'
 
-$deploymentName = 'sp' + $srNumber
+$deploymentName = 'sp-' + $srNumber
 $storageAccountNamePrefix  = 'storage' + $srNumber
 
 $sharepointFarmName = 'spfarm' + $srNumber
 $virtualNetworkName = 'spfarmVNET'+ $srNumber
-$sppuplicIP = 'sppuplicIP'
+#$sppuplicIP = 'sppuplicIP'
+
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $Location -Verbose -Force
+
+$sppublicIP = New-AzureRmPublicIpAddress -AllocationMethod Dynamic -ResourceGroupName $resourceGroupName -Name $sppuplicIPName  -Location $location
+
 $parameters=@{
 
 
@@ -51,9 +58,9 @@ sharePointFarmAccountUserName='sp_farm'
 sharePointFarmAccountPassword= 'P@ssw0rd1234'
 sharePointFarmPassphrasePassword='P@ssw0rd1234'
 spSiteTemplateName='STS#0'
-spDNSPrefix='sp-unique'
+spDNSPrefix='sp-unique' + $srNumber
 #baseUrl='https://raw.githubusercontent.com/aayad/sharepoint-three-vm/master'
-baseUrl = 'https://raw.githubusercontent.com/aqasrawi/sp2016/master'
+baseUrl = 'https://raw.githubusercontent.com/aqasrawi/sp2013/master'
 #baseUrl = 'https://raw.githubusercontent.com/razar-msft/SharePoint-Non-HA-FARM/master'
 spPublicIPNewOrExisting='new'
 spPublicIPRGName=''
@@ -63,13 +70,13 @@ storageAccountType='Standard_LRS'
 
 }
 
-New-AzureRmResourceGroup `
-    -Name $resourceGroupName `
-    -Location $Location `
-    -Verbose -Force
+#New-AzureRmResourceGroup `
+#    -Name $resourceGroupName `
+#    -Location $Location `
+#    -Verbose -Force
 
 
 
-New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile "C:\Users\aqasrawi\OneDrive - Microsoft\Engagements\BoC Azure\sharepoint-three-vm-master\azuredeploy.json" -TemplateParameterObject $parameters -Verbose
+New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile "C:\Users\aqasrawi\Documents\GitHub\SP2013\azuredeploy.json" -TemplateParameterObject $parameters -Verbose
 
 
